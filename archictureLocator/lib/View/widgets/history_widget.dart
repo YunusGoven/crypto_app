eea1c13facfcbef4a740d63c7d1b890af5210c0f
Hugues COLLIGNON <h.collignon@student.helmo.be>
2021-12-26 18:08:47 +0100

    end History

--- archictureLocator/lib/View/widgets/history_widget.dart
+++ archictureLocator/lib/View/widgets/history_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:mvvm/Model/models/history.dart';
 
 class HistoryWidget extends StatelessWidget {
@@ -7,40 +8,52 @@ class HistoryWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(crossAxisAlignment: CrossAxisAlignment.center, children: [
-      Text(
-        model.type,
-        style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
-      ),
-      SizedBox(
-        width: 10,
-      ),
-      Expanded(
-        child: Container(
-          decoration: BoxDecoration(
-            border: Border.all(
-              color: Colors.black,
-            ),
-            borderRadius: BorderRadius.circular(10.0),
-          ),
-          child: Padding(
-            padding: EdgeInsets.only(left: 10, right: 30, top: 10, bottom: 10),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  "${model.onTransactionValue} \$",
-                  style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
-                ),
-                Text(
-                  model.number.toString() + " " + model.cryptoName,
-                  style: TextStyle(fontSize: 19, fontWeight: FontWeight.bold),
-                ),
-              ],
-            ),
-          ),
+    DateTime ui = DateTime.tryParse(model.dateTransaction);
+    String formated = DateFormat('dd-MM-yyyy').format(ui);
+    return Container(
+      padding: EdgeInsets.only(bottom: 10, left: 40, top: 10, right: 40),
+      decoration: BoxDecoration(
+        border: Border.all(
+          color: Colors.black,
+          width: 5,
         ),
       ),
-    ]);
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Column(
+            children: [
+              Text(
+                model.type,
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 26),
+              ),
+              Text(formated),
+            ],
+          ),
+          Text(
+            model.type == "VENTE"
+                ? model.number.toString() + model.cryptoId
+                : model.value.toString() + model.cryptoId,
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 35),
+          ),
+          Column(
+            children: [
+              Text(
+                model.type == "VENTE"
+                    ? model.value.toString() + "\$"
+                    : model.number.toString() + "\$",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+              ),
+              SizedBox(
+                height: 5,
+              ),
+              Text(model.onTransactionValue.toString() +
+                  "\$ /1 " +
+                  model.cryptoId),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
--- archictureLocator/lib/ViewModel/wallet_viewmodel.dart
+++ archictureLocator/lib/ViewModel/wallet_viewmodel.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:mvvm/Model/models/crypto.dart';
 import 'package:mvvm/Model/models/wallet.dart';
 import 'package:mvvm/Services/api_service.dart';
 import 'package:mvvm/locator.dart';
