cb6c9ab1eb355066b0bb81bad63d99b8a992a2de
Yunus Goven <y.goven@student.helmo.be>
2021-12-28 20:07:30 +0100

    beautiful graph

--- archictureLocator/lib/View/components/detail_graph_widget.dart
+++ archictureLocator/lib/View/components/detail_graph_widget.dart
@@ -1,30 +1,67 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:mvvm/Model/models/crypto.dart';
 import 'package:syncfusion_flutter_charts/charts.dart';
 
-class Graph extends StatelessWidget {
+class Graph extends StatefulWidget {
   final Crypto crypto;
   const Graph({Key key, @required this.crypto}) : super(key: key);
+
+  @override
+  _GraphState createState() => _GraphState();
+}
+
+class _GraphState extends State<Graph> {
+  CrosshairBehavior _crosshairBehavior;
+  TooltipBehavior _tooltipBehavior;
+
+  @override
+  void initState() {
+    _tooltipBehavior = TooltipBehavior(enable: true);
+    _crosshairBehavior = CrosshairBehavior(
+        enable: true,
+        lineColor: Colors.red,
+        lineDashArray: <double>[5, 5],
+        lineWidth: 2,
+        activationMode: ActivationMode.singleTap);
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     List<HistoryData> data = getChartData();
 
     return SfCartesianChart(
+      plotAreaBorderWidth: 1,
+      plotAreaBorderColor: Colors.black,
+      tooltipBehavior: _tooltipBehavior,
+      crosshairBehavior: _crosshairBehavior,
+      primaryXAxis: DateTimeAxis(
+          majorGridLines:
+              const MajorGridLines(width: 1, dashArray: <double>[5, 5]),
+          title: AxisTitle(text: "Jour"),
+          dateFormat: DateFormat("d/M"),
+          rangePadding: ChartRangePadding.round),
+      primaryYAxis: NumericAxis(
+          title: AxisTitle(text: "Valeur \$"),
+          decimalPlaces: 4,
+          rangePadding: ChartRangePadding.additional),
       series: <ChartSeries>[
-        LineSeries<HistoryData, dynamic>(
-          dataSource: data,
-          xValueMapper: (HistoryData datas, _) =>
-              DateTime.tryParse(datas.date).hour,
-          yValueMapper: (HistoryData datas, _) => datas.value,
-        ),
+        SplineSeries<HistoryData, DateTime>(
+            color: Colors.grey[850],
+            enableTooltip: true,
+            dataSource: data,
+            xValueMapper: (HistoryData datas, _) =>
+                DateTime.tryParse(datas.date),
+            yValueMapper: (HistoryData datas, _) => datas.value,
+            dataLabelSettings: DataLabelSettings(isVisible: true)),
       ],
     );
   }
 
   List<HistoryData> getChartData() {
     List<HistoryData> data = [];
-    crypto.HistoryValue.forEach((key, value) {
+    widget.crypto.HistoryValue.forEach((key, value) {
       var h = HistoryData(key, value);
       data.add(h);
     });
